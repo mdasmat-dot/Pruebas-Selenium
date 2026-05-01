478cb3901179ea61b0179196bb7db1683c0ea24c
Mario Davila <mdasmat@gmail.com>
2026-05-01 13:18:57 -0500

    Tercera Actualizacion

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import org.jspecify.annotations.Nullable;
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -20,19 +17,18 @@ public class Main {
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://auth.wikimedia.org/eswiki/wiki/Especial:Crear_una_cuenta?useformat=desktop&usesul3=1&returnto=Selenium&centralauthLoginToken=8893a474fb57dbff9e099888a3484fc1");
+        driver.get("https://onecompiler.com/html/3xfmfsrwk");
 
-        WebElement usuario = driver.findElement(By.id("wpName2"));
-        usuario.sendKeys("usuario Automation");
+        WebElement alertabutton = driver.findElement(By.xpath("/html/body/div[4]/div/div/div/div[1]/div[3]/div/button[3]"));
 
-        WebElement contraseña = driver.findElement(By.id("wpPassword2"));
-        contraseña.sendKeys("contraseñaAutomation");
+        alertabutton.click();
 
-        WebElement contraseñar = driver.findElement(By.id("wpRetype"));
-        contraseñar.sendKeys("contraseñaAutomation");
+        WebDriverWait esperar = new WebDriverWait(driver, Duration.ofSeconds(10));
+        esperar.until(ExpectedConditions.alertIsPresent());
 
-        WebElement correo = driver.findElement(By.id("wpEmail"));
-        correo.sendKeys("correo1@gmail.com");
+        Alert alert = driver.switchTo().alert();
+        System.out.println(alert.getText());
+        alert.accept();
 
 
     }
